ba7e6ffbabb4ca2ed818dd0e238ed8d8f8a4ae01
Md Salman <salman6ceal@gmail.com>
2021-05-28 03:30:29 +0530

    Adding Covid Data and Gifs

--- app.js
+++ app.js
@@ -10,21 +10,58 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+// Covid 19 API call
+let covidResult = {};
+let confirmed, recovered, deaths;
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const options = { method : 'GET' };
+const covidUrl = "https://covid19.mathdro.id/api/countries/India/";
+const request1 = https.request(covidUrl, options, function(response){
+  //Here response sent by the CoWin API
+
+  if(response.statusCode === 200)
+  {
+    response.on('data', function(data) {
+
+      covidResult = JSON.parse(data);
+
+      confirmed = numberWithCommas(covidResult.confirmed.value);
+      recovered = numberWithCommas(covidResult.recovered.value);
+      deaths = numberWithCommas(covidResult.deaths.value);
+      // console.log(confirmed,recovered,deaths);
+    });
+     
+  }
+  else
+  console.log("error");
+ 
+}); //https.request()
+request1.end();
+
 app.get("/", function(req,res){
-  res.render("main");
+  res.render("main", {confirmed:confirmed, recovered:recovered, deaths:deaths});
 });
 
 
 app.post("/", function(req,res){
 
-  const pincode = req.body.Pincode;
-  // console.log(pincode);
+const pincode = req.body.Pincode;
 
-  const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pincode + "&date=26-05-2021";
+let d = new Date();
+let date = d.getDate();
+let month = d.getMonth()+1;
+let year = d.getFullYear();
+let currDate = date+"-"+month+"-"+year;
+  
+const cowinUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pincode + "&date=" + currDate;
 
   const options = { method : 'GET' };
   
-    const request = https.request(url, options, function(response){
+    const request = https.request(cowinUrl, options, function(response){
             //Here response sent by the CoWin API
   
             let chunks = [];
@@ -38,33 +75,58 @@ app.post("/", function(req,res){
                 
                   if(result.length === 0)
                   {
-                    res.send("Currently No Slots Available....");
+                    res.render("failure", {content : "Currently No Slots are available. Pleasr Try Again Later.", 
+                    confirmed:confirmed, recovered:recovered, deaths:deaths});
                   }
                   else
                   {
                     // res.send(result);
-                    res.render("home",{data : result}); 
+                    res.render("home",{data : result, confirmed:confirmed, recovered:recovered, deaths:deaths}); 
                   }
               });
-
-              // response.on("data", function(data){
-              //   if(data.length)
-              //   {
-              //     const result = JSON.parse(data).sessions;
-              //     res.send(result);
-  
-              //     result.forEach(function(i){
-              //       console.log(i.name);
-              //     });
-              //   }
-              // }); //response.on()
             }
             else
             {
-              res.sendFile(__dirname + "/failure.html"); 
+              res.render("failure",{content : "Please Enter Correct Pincode.", confirmed:confirmed, recovered:recovered, deaths:deaths}); 
             }   
       }); //https.request()
       request.end();
+
+
+
+  //     function numberWithCommas(x) {
+  //       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  //     }
+
+
+
+  //     // Covid 19 API call
+  //     const covidUrl = "https://covid19.mathdro.id/api/countries/India/";
+  //     const request1 = https.request(covidUrl, options, function(response){
+  //       //Here response sent by the CoWin API
+
+  //       if(response.statusCode === 200)
+  //       {
+  //         response.on('data', function(data) {
+
+  //           const result = JSON.parse(data);
+
+  //           const confirmed = numberWithCommas(result.confirmed.value);
+  //           const recovered = numberWithCommas(result.recovered.value);
+  //           const deaths = numberWithCommas(result.deaths.value);
+  //           console.log(confirmed,recovered,deaths);
+  //         });
+           
+  //       }
+  //       else
+  //       console.log("error");
+
+      
+         
+  // }); //https.request()
+  // request1.end();
+
+     
 
     }); //app.post()
 
@@ -96,6 +158,6 @@ app.post("/", function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server running on port 3000....")
+app.listen(process.env.PORT || 4000, function(){
+  console.log("Server running on port 4000....")
 });
