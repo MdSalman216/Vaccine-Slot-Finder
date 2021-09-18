6c6566e44a2d17b610bad399a6e67e1f30da99ac
MdSalman216 <salman6ceal@gmail.com>
2021-09-18 14:13:29 +0530

    Changed the API End Point for Covid Tracker

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const request = require("request");
 const https = require("https");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Covid 19 API call
 let covidResult = {};
@@ -17,87 +16,104 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-const options = { method : 'GET' };
-const covidUrl = "https://covid19.mathdro.id/api/countries/India/";
-const request1 = https.request(covidUrl, options, function(response){
-  //Here response sent by the CoWin API
+const options = { method: "GET" };
+// const covidUrl = "https://covid19.mathdro.id/api/countries/India/";
+const covidUrl = "https://api.rootnet.in/covid19-in/stats/latest";
 
-  if(response.statusCode === 200)
-  {
-    response.on('data', function(data) {
+const request1 = https.request(covidUrl, options, function (response) {
+  let chunks = [];
 
-      covidResult = JSON.parse(data);
+  if (response.statusCode === 200) {
+    response
+      .on("data", function (data) {
+        chunks.push(data);
+      })
+      .on("end", function () {
+        let data = Buffer.concat(chunks);
+        covidResult = JSON.parse(data);
 
-      confirmed = numberWithCommas(covidResult.confirmed.value);
-      recovered = numberWithCommas(covidResult.recovered.value);
-      deaths = numberWithCommas(covidResult.deaths.value);
-      // console.log(covidResult);
-    });
-     
+        confirmed = numberWithCommas(covidResult.data.summary.total);
+        recovered = numberWithCommas(covidResult.data.summary.discharged);
+        deaths = numberWithCommas(covidResult.data.summary.deaths);
+
+        console.log(covidResult.data.summary);
+      });
   }
-  else
-  console.log("error");
- 
 }); //https.request()
 request1.end();
 
-app.get("/", function(req,res){
-  res.render("main", {confirmed:confirmed, recovered:recovered, deaths:deaths});
+app.get("/", function (req, res) {
+  res.render("main", {
+    confirmed: confirmed,
+    recovered: recovered,
+    deaths: deaths,
+  });
 });
 
+app.post("/", function (req, res) {
+  const pincode = req.body.Pincode;
 
-app.post("/", function(req,res){
+  let d = new Date();
+  let date = d.getDate();
+  let month = d.getMonth() + 1;
+  let year = d.getFullYear();
+  let currDate = date + "-" + month + "-" + year;
 
-const pincode = req.body.Pincode;
+  const cowinUrl =
+    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" +
+    pincode +
+    "&date=" +
+    currDate;
 
-let d = new Date();
-let date = d.getDate();
-let month = d.getMonth()+1;
-let year = d.getFullYear();
-let currDate = date+"-"+month+"-"+year;
-  
-const cowinUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+pincode+"&date=" + currDate;
+  const options = { method: "GET" };
 
-  const options = { method : 'GET' };
-  
-    const request = https.request(cowinUrl, options, function(response){
-            //Here response sent by the CoWin API
-  
-            let chunks = [];
-            if(response.statusCode === 200)
-            {
-              response.on('data', function(data) {
-                chunks.push(data);
-              }).on('end', function() {
-                let data = Buffer.concat(chunks);
-                let result = JSON.parse(data).sessions;
-                
-                  if(result.length === 0)
-                  {
-                    res.render("failure", {content : "Currently No Slots are available. Pleasr Try Again Later.", 
-                    confirmed:confirmed, recovered:recovered, deaths:deaths});
-                  }
-                  else
-                  {
-                    // res.send(result);
-                    res.render("home",{data : result, confirmed:confirmed, recovered:recovered, deaths:deaths}); 
-                  }
-              });
-            }
-            else
-            {
-              res.render("failure",{content : "OOPs! Government restricts API for third party integration.", confirmed:confirmed, recovered:recovered, deaths:deaths}); 
-            }   
-      }); //https.request()
-      request.end();
+  const request = https.request(cowinUrl, options, function (response) {
+    //Here response sent by the CoWin API
 
-    }); //app.post()
+    let chunks = [];
+    if (response.statusCode === 200) {
+      response
+        .on("data", function (data) {
+          chunks.push(data);
+        })
+        .on("end", function () {
+          let data = Buffer.concat(chunks);
+          let result = JSON.parse(data).sessions;
 
-    app.post("/failure", function(req,res){
-      res.redirect("/");
-    });
+          if (result.length === 0) {
+            res.render("failure", {
+              content:
+                "Currently No Slots are available. Pleasr Try Again Later.",
+              confirmed: confirmed,
+              recovered: recovered,
+              deaths: deaths,
+            });
+          } else {
+            // res.send(result);
+            res.render("home", {
+              data: result,
+              confirmed: confirmed,
+              recovered: recovered,
+              deaths: deaths,
+            });
+          }
+        });
+    } else {
+      res.render("failure", {
+        content: "OOPs! Government restricts API for third party integration.",
+        confirmed: confirmed,
+        recovered: recovered,
+        deaths: deaths,
+      });
+    }
+  }); //https.request()
+  request.end();
+}); //app.post()
 
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
 
-    app.listen(process.env.PORT || 3000, function(){
-      console.log("Server Started on port 3000...")
-    });
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server Started on port 3000...");
+});
